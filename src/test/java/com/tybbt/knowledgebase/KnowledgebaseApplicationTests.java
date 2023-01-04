33b4a683026aefa8ebf077fbd3b7d2e20d5d8b2d
ty <875260315@qq.com>
2023-01-04 09:14:05 +0800

    2-2 init project setteing

---
+++ src/test/java/com/tybbt/knowledgebase/KnowledgebaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tybbt.knowledgebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgebaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
