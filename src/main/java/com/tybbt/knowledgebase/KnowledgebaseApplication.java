5c255a930e59fb3ed6c8f6646146a20c34deced4
ty <875260315@qq.com>
2023-01-04 10:19:43 +0800

    2-5 change starting text

--- src/main/java/com/tybbt/knowledgebase/KnowledgebaseApplication.java
+++ src/main/java/com/tybbt/knowledgebase/KnowledgebaseApplication.java
@@ -1,13 +1,21 @@
 package com.tybbt.knowledgebase;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class KnowledgebaseApplication {
-
+    private static final Logger LOG = LoggerFactory.getLogger(KnowledgebaseApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(KnowledgebaseApplication.class, args);
+
+        //SpringApplication.run(KnowledgebaseApplication.class, args);
+        SpringApplication app = new SpringApplication(KnowledgebaseApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
 
 }
