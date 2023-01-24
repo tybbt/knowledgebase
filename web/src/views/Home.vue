69a4e790261ffbb5e32d2c9a4801c2215811bf85
ty <875260315@qq.com>
2023-01-24 11:39:07 +0800

    5-4 集成ant-design vue 列表视图，在maints中加入全局icon引入

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -44,37 +44,91 @@
       </a-menu>
     </a-layout-sider>
     <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
-      <pre>
-{{ ebooks }}
-{{ book_list }}
-      </pre>
+      <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
+        <template #footer>
+          <div>
+            <b>ant design vue</b>
+            footer part
+          </div>
+        </template>
+        <template #renderItem="{ item }">
+          <a-list-item key="item.title">
+            <template #actions>
+        <span v-for="{ type, text } in actions" :key="type">
+          <component v-bind:is="type" style="margin-right: 8px" />
+          {{ text }}
+        </span>
+            </template>
+            <template #extra>
+              <img
+                  width="272"
+                  alt="logo"
+                  src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+              />
+            </template>
+            <a-list-item-meta :description="item.description">
+              <template #title>
+                <a :href="item.href">{{ item.title }}</a>
+              </template>
+              <template #avatar><a-avatar :src="item.avatar" /></template>
+            </a-list-item-meta>
+            {{ item.content }}
+          </a-list-item>
+        </template>
+      </a-list>
     </a-layout-content>
   </a-layout>
 </template>
 
 <script>
-import {defineComponent, onMounted, ref, reactive, toRef } from "vue";
+import {defineComponent, onMounted, ref } from "vue";
 import axios from 'axios';
+
+const listData = [];
+
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: 'https://www.antdv.com/',
+    title: `ant design vue part ${i}`,
+    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+    description:
+        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:
+        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  });
+}
 
 export default defineComponent({
   name: 'Home',
   setup() {
     console.log("set up");
     const ebooks = ref();
-    const ebook_re = reactive({books: []});
+    // const ebook_re = reactive({books: []});
 
     onMounted(() => {
       console.log("onMounted");
       axios.get("http://localhost:8080/ebook/list?name=Spring").then((response) => {
         const data = response.data;
         ebooks.value = data.content;
-        ebook_re.books = data.content;
+        // ebook_re.books = data.content;
         console.log(response);
       });
     });
     return {
       ebooks,
-      book_list: toRef(ebook_re, "books")
+      // book_list: toRef(ebook_re, "books")
+      listData,
+      pagination : {
+        onChange: page => {
+          console.log(page);
+        },
+        pageSize: 3,
+      },
+      actions: [
+        { type: 'StarOutlined', text: '156' },
+        { type: 'LikeOutlined', text: '156' },
+        { type: 'MessageOutlined', text: '2' },
+      ]
     };
   }
 });
