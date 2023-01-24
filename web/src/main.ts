dbd660b9058d0279e4a669b10422333daf2ec9fd
ty <875260315@qq.com>
2023-01-24 12:31:46 +0800

    5-5 vue 多环境配置，编译和启动添加dev和prod

--- web/src/main.ts
+++ web/src/main.ts
@@ -5,6 +5,7 @@ import store from './store'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 import * as Icons from '@ant-design/icons-vue';
+import * as process from "process";
 
 const app = createApp(App);
 app.use(store).use(router).use(Antd).mount('#app');
@@ -13,3 +14,6 @@ const icons: any = Icons;
 for (const i in icons) {
     app.component(i, icons[i]);
 }
+
+console.log('环境:', process.env.NODE_ENV)
+console.log('IP:', process.env.VUE_APP_SERVER)
