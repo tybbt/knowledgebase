52b3acb273ac3d38aa48cb2f98b7fe237de3ed2c
ty <875260315@qq.com>
2023-01-24 18:06:54 +0800

    5-8 使用AOP，利用切点（Pointcut）加通知（前置通知Before、后置通知After、环绕通知Around）实现一个切面，利用切面添加日志。Filter、Interceptor、AOP三选一即可,选择AOP，注释掉Filter、Interceptor和mvcConfig

--- src/main/java/com/tybbt/knowledgebase/filter/LogFilter.java
+++ src/main/java/com/tybbt/knowledgebase/filter/LogFilter.java
@@ -1,32 +1,32 @@
-package com.tybbt.knowledgebase.filter;
-
-import jakarta.servlet.*;
-import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-
-import java.io.IOException;
-
-@Component
-public class LogFilter implements Filter {
-    private static final Logger LOG = LoggerFactory.getLogger(LogFilter.class);
-
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-
-    }
-
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        // Filter作用在容器上（Tomcat），启动后首先进入filter，拦截器interceptor作用在应用SpringBoot（web应用）上，被拦截器拿到，然后最后进入业务逻辑
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        LOG.info("------------- LogFilter 开始 -------------");
-        LOG.info("请求地址: {} {}", request.getRequestURL().toString(), request.getMethod());
-        LOG.info("远程地址: {}", request.getRemoteAddr());
-
-        long startTime = System.currentTimeMillis();
-        filterChain.doFilter(servletRequest, servletResponse);
-        LOG.info("------------- LogFilter 结束 耗时：{} ms -------------", System.currentTimeMillis() - startTime);
-    }
-}
+//package com.tybbt.knowledgebase.filter;
+//
+//import jakarta.servlet.*;
+//import jakarta.servlet.http.HttpServletRequest;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.stereotype.Component;
+//
+//import java.io.IOException;
+//
+//@Component
+//public class LogFilter implements Filter {
+//    private static final Logger LOG = LoggerFactory.getLogger(LogFilter.class);
+//
+//    @Override
+//    public void init(FilterConfig filterConfig) throws ServletException {
+//
+//    }
+//
+//    @Override
+//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//        // Filter作用在容器上（Tomcat），启动后首先进入filter，拦截器interceptor作用在应用SpringBoot（web应用）上，被拦截器拿到，然后最后进入业务逻辑
+//        HttpServletRequest request = (HttpServletRequest) servletRequest;
+//        LOG.info("------------- LogFilter 开始 -------------");
+//        LOG.info("请求地址: {} {}", request.getRequestURL().toString(), request.getMethod());
+//        LOG.info("远程地址: {}", request.getRemoteAddr());
+//
+//        long startTime = System.currentTimeMillis();
+//        filterChain.doFilter(servletRequest, servletResponse);
+//        LOG.info("------------- LogFilter 结束 耗时：{} ms -------------", System.currentTimeMillis() - startTime);
+//    }
+//}
