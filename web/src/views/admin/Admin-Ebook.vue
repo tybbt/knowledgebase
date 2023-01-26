4730ba1287cad8de9a43c5d8fce97551c7560b08
ty <875260315@qq.com>
2023-01-26 12:24:45 +0800

    6-12 优化编辑时的引用传递带来的直接前端页面变化，利用值传递保护前端

--- web/src/views/admin/Admin-Ebook.vue
+++ web/src/views/admin/Admin-Ebook.vue
@@ -84,6 +84,7 @@
   import { defineComponent, onMounted, ref } from 'vue';
   import axios from 'axios';
   import {message} from "ant-design-vue";
+  import {Tool} from "@/util/tool";
 
   export default defineComponent({
     name: 'AdminEbook',
@@ -206,7 +207,7 @@
        */
       const edit = (record: any) => {
         modelVisible.value = true;
-        ebook.value = record;
+        ebook.value = Tool.copy(record);
       };
 
       const add = () => {
