22cd56978a1b3d565745e7a66df75681c9e6e47f
ty <875260315@qq.com>
2023-01-31 21:29:26 +0800

    8-12 打开无文档的电子书时提示当前电子书无文档

--- web/src/views/Doc.vue
+++ web/src/views/Doc.vue
@@ -2,6 +2,7 @@
   <a-layout>
     <a-layout-content
       :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
+      <h3 v-if="level1.length === 0">对不起，找不到相关文档！相关内容丢失或该电子书下没有任何文档。</h3>
       <a-row>
         <a-col :span="6">
           <a-tree
