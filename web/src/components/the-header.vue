7b583765cdd309bb7ebb6c888e1919e62b51eb7a
ty <875260315@qq.com>
2023-02-01 14:26:57 +0800

    9-8 增加前端登录界面

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -2,6 +2,7 @@
   <a-layout-header class="header">
     <div class="logo">
     </div>
+
     <a-menu
         theme="dark"
         mode="horizontal"
@@ -43,14 +44,78 @@
           关于我们
         </router-link>
       </a-menu-item>
+
+      <a class="login-menu" @click="showLoginModal">
+        <span>
+          <UserOutlined />
+          登录
+        </span>
+      </a>
+
     </a-menu>
+
+    <a-modal
+      title="登录"
+      v-model:visible="loginModalVisible"
+      :confirm-loading="loginModalLoading"
+      @ok="login"
+    >
+      <a-form :model="loginUser" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
+
+        <a-form-item label="登录名">
+          <a-input v-model:value="loginUser.loginName" />
+        </a-form-item>
+
+        <a-form-item label="密码">
+          <a-input v-model:value="loginUser.password" type="password" />
+        </a-form-item>
+
+      </a-form>
+    </a-modal>
+
   </a-layout-header>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import {defineComponent, ref} from 'vue';
 
 export default defineComponent({
-  name: 'the-header'
+  name: 'the-header',
+  setup () {
+    const loginUser = ref({
+      loginName: "test",
+      password: "test"
+    });
+
+    const loginModalVisible = ref(false);
+    const loginModalLoading = ref(false);
+
+    const showLoginModal = () => {
+      loginModalVisible.value = true;
+    }
+
+    const login = () => {
+      console.log("开始登陆");
+    }
+
+    return {
+      loginUser,
+      loginModalVisible,
+      loginModalLoading,
+      showLoginModal,
+      login
+    }
+  }
+
+
 });
 </script>
+
+<style>
+  .login-menu {
+    float: right !important;
+    color: white;
+    padding-left: 10px;
+    margin-right: 10px;
+  }
+</style>
