88a3c5d3fab0f4636b9a0d5e66c1eebbd38e2494
ty <875260315@qq.com>
2023-02-03 15:08:00 +0800

    11-10 修改并展示30天趋势折线图

--- src/main/java/com/tybbt/knowledgebase/job/EbookSnapshotJob.java
+++ src/main/java/com/tybbt/knowledgebase/job/EbookSnapshotJob.java
@@ -21,7 +21,7 @@ public class EbookSnapshotJob {
      * 自定义cron表达式跑批
      * 每30秒更新电子书信息
      */
-    @Scheduled(cron = "0 0 * * * ?")
+    @Scheduled(cron = "0/30 * * * * ?")
     public void cron() {
         // 增加日志流水号
         MDC.put("LOG_ID", String.valueOf(snowFlake.nextId()));
