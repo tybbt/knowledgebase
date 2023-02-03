f3eba3a93f522a1c1866514f69584aa1d73f7957
ty <875260315@qq.com>
2023-02-03 11:35:28 +0800

    11-4 电子书快照增加唯一键，增加今日阅读数、今日点赞数SQL的编写方法

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -102,8 +102,8 @@ drop table if exists user;
 create table user (
     id bigint not null comment 'ID',
     login_name varchar(50) not null comment '登录名',
-    name varchar(50) comment '昵称',
-    password char(32) not null comment '密码',
+    `name` varchar(50) comment '昵称',
+    `password` char(32) not null comment '密码',
     primary key (id),
     unique key login_name_unique (login_name)
 ) engine=innodb default charset=utf8mb4 comment='用户';
@@ -111,12 +111,13 @@ create table user (
 
 drop table if exists ebook_snapshot;
 create table ebook_snapshot (
-    id bigint auto_increment not null comment 'id',
-    ebook_id bigint not null default 0 comment '电子书id',
-    date date not null comment '快照日期',
-    view_count int not null default 0 comment '阅读数',
-    vote_count int not null default 0 comment '点赞数',
-    view_increase int not null default 0 comment '阅读增长',
-    vote_increase int not null default 0 comment '点赞增长',
-    primary key (id)
+    `id` bigint auto_increment not null comment 'id',
+    `ebook_id` bigint not null default 0 comment '电子书id',
+    `date` date not null comment '快照日期',
+    `view_count` int not null default 0 comment '阅读数',
+    `vote_count` int not null default 0 comment '点赞数',
+    `view_increase` int not null default 0 comment '阅读增长',
+    `vote_increase` int not null default 0 comment '点赞增长',
+    primary key (`id`),
+    unique key ebook_id_date_unique (`ebook_id`, `date`)
 ) engine=innodb default charset = utf8mb4 comment ='电子书快照表';
